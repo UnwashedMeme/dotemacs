6f2c83c71eb1b4069e9cb909cbbce821476381fc
Nathan Bird <nathan@acceleration.net>
2011-10-03 13:59:23 -0400

    Better slime filename translations.

--- settings/slime-settings.el
+++ settings/slime-settings.el
@@ -1,33 +1,31 @@
 
 ;;;;;;;;;;;;; slime-setup ;;;;;;;;;;;;;;
 (if (equal (system-name) "nathan-ubuntu")
-    (load "/home/ACCELERATION/nathan/projects/lisp/ADWCodeBase/misc/shared-slime-init.el")
+    (load (expand-file-name "~/projects/lisp/ADWCodeBase/misc/shared-slime-init.el"))
   (progn
     (add-to-list 'load-path "S:/slime")
     (autoload 'slime "S:/home/nathan/lisp/ADWCodeBase/misc/shared-slime-init.el" nil t)
     (autoload 'slime-connect "S:/home/nathan/lisp/ADWCodeBase/misc/shared-slime-init.el" nil t)
     ))
 
+(push 
+ (slime-filename-translation-mount-map
+  "progden" '(("/home/ACCELERATION/" . "/mnt/progden/home/")
+              ("/usr/local/share/common-lisp/source/" . "/mnt/progden/site/")
+              ("/opt/lisp" . "/mnt/progden")
+              ("/var/git" . "/mnt/progden/gitroot")
+              ("/var/darcs" . "/mnt/progden/darcs-root")))
+ slime-filename-translations)
 
-(lexical-let ((progden-lisp-map
-               '(("/home/ACCELERATION/" . "/mnt/progden/home/")
-                 ("/usr/local/share/common-lisp/source/" . "/mnt/progden/site/")
-                 ("/opt/lisp" . "/mnt/progden")
-                 ("/var/git" . "/mnt/progden/gitroot")
-                 ("/var/darcs" . "/mnt/progden/darcs-root"))))
-(push (list "progden"
-            (lambda (local-filename)
-              (setf local-filename (replace-in-string local-filename "\\\\" "/"))
-              (let ((the-map (find-if (lambda (amap)
-                                        (string-starts-with local-filename (shared-lisp-map-local amap)))
-                                      progden-lisp-map)))
-                (concat (shared-lisp-map-remote the-map)
-                        (substring local-filename (length (shared-lisp-map-local the-map))))))
-            (lambda (remote-filename)
-              (let ((the-map (find-if (lambda (amap)
-                                        (string-starts-with remote-filename (shared-lisp-map-remote amap)))
-                                      progden-lisp-map)))
-                (concat (shared-lisp-map-local the-map)
-                        (substring remote-filename
-                                   (length (shared-lisp-map-remote the-map)))))))
-      slime-filename-translations))
+;; (destructuring-bind (name local remote)
+;;     (slime-filename-translation-mount-map
+;;      "progden" '(("/home/ACCELERATION/" . "/mnt/progden/home/")
+;;                  ("/usr/local/share/common-lisp/source/" . "/mnt/progden/site/")
+;;                  ("/var/git" . "/mnt/progden/gitroot")
+;;                  ("/var/darcs" . "/mnt/progden/darcs-root")
+;;                  ("/opt/lisp" . "/mnt/progden")))
+;;   (list (funcall remote     "/var/git/clsql-helper/date.lisp")
+;;         (funcall local "/mnt/progden/gitroot/clsql")
+;;         (funcall remote "/something/weird")))
+
+;(slime-tramp-from-lisp-filename "/var/git/clsql-helper/date.lisp")
