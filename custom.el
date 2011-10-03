4efe2f04809e0fa9d5ddcef74115caa4ba86ff1d
Nathan Bird <nathan@acceleration.net>
2011-10-03 14:06:17 -0400

    Getting .emacs.d cleaned up from git's perspective

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(ac-comphist-file "/home/ACCELERATION/nathan/.emacs.d/persistence/ac-comphist.dat")
  '(backup-directory-alist (quote (("." . "~/.emacs.d/persistence/backups"))))
  '(clean-buffer-list-delay-general 4)
  '(column-number-mode t)
