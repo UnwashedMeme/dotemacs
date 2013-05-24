6ddde76f722a54c7736ad935d4508361328e6c53
Nathan Bird <nathan@acceleration.net>
2013-05-24 16:49:08 -0400

    updating emacs config

--- site-lisp/mypylint.el
+++ site-lisp/mypylint.el
@@ -4,8 +4,7 @@
 (defun mypylint-reset-ignore ()
   "Reset the list of ignored pep8/pylint error codes to none."
   (interactive)
-  (setf mypylint-ignore-message-ids nil)
-  (my-flymake-build-ignore))
+  (setf mypylint-ignore-message-ids nil))
 
 (defun mypylint-ignore-message (message-id)
   (interactive 
@@ -58,60 +57,67 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; flymake-python-pyflakes minor mode for easing navigation.
-
 (setq flymake-python-pyflakes-executable "flake8")
+
+(defvar my-pyflakes-ignore-message-ids nil 
+  "List of flake8 message ids to ignore.")
 
 ;;; Define a minor mode to help navigate around
 ;;; Cribbed from http://www.emacswiki.org/emacs/FlyMake
-(defvar my-flymake-minor-mode-map
+(defvar my-pyflakes-minor-mode-map
   (let ((map (make-sparse-keymap)))
     (define-key map "\M-p" 'flymake-goto-prev-error)
     (define-key map "\M-n" 'flymake-goto-next-error)
-    (define-key map (kbd "C-c i") 'my-flymake-ignore-error)
+    (define-key map (kbd "C-c i") 'my-pyflakes-ignore-error)
     map)
   "Keymap for my flymake minor mode.")
 
-(define-minor-mode my-flymake-minor-mode
+(define-minor-mode my-pyflakes-minor-mode
   "Simple minor mode which adds some key bindings for moving to the next and previous errors.
 
 Key bindings:
 
-\\{my-flymake-minor-mode-map}"
+\\{my-pyflakes-minor-mode-map}"
   nil
   nil
-  :keymap my-flymake-minor-mode-map)
+  :keymap my-pyflakes-minor-mode-map)
 
-(defun my-flymake-get-help ()
+(defun my-pyflakes-get-help ()
   "Find the flymake help text for the current point"
   (when (get-char-property (point) 'flymake-overlay)
     (let ((help (get-char-property (point) 'help-echo)))
       help)))
 
-(defun my-flymake-show-help ()
+(defun my-pyflakes-show-help ()
   "Print in the minibuffer the help text for highlighted error at the current point."
-  (let ((help (my-flymake-get-help)))
+  (let ((help (my-pyflakes-get-help)))
     (when help (message "%s" help))))
 
 ;;; run the show-help function after each command so that is is always up to date.
-(add-hook 'post-command-hook 'my-flymake-show-help)
+(add-hook 'post-command-hook 'my-pyflakes-show-help)
 
-(defun my-flymake-ignore-error ()
+(defun my-pyflakes-ignore-error ()
   "Ignore the error code that is highlighting the current point"
   (interactive)
-  (let ((help (my-flymake-get-help)))
+  (let ((help (my-pyflakes-get-help)))
     (message "help was: %s" help)
     (when (and help (string-match "^\\([CWRE][0-9]+\\)" help))
       (let ((message-id (match-string 1 help)))
-        (pushnew message-id mypylint-ignore-message-ids :test 'string=)
-        (my-flymake-build-ignore)
+        (pushnew message-id my-pyflakes-ignore-message-ids :test 'string=)
+        (my-pyflakes-build-ignore)
         (message "Ignored: %s" message-id)
         ))))
 
-(defun my-flymake-build-ignore ()
+(defun my-pyflakes-build-ignore ()
   "Build the `--ignore=... argument out of our ignored codes.`"
   (setq flymake-python-pyflakes-extra-arguments
-        (when mypylint-ignore-message-ids
-            (list (concat "--ignore=" (mapconcat 'identity mypylint-ignore-message-ids ",")))))
+        (when my-pyflakes-ignore-message-ids
+            (list (concat "--ignore=" (mapconcat 'identity my-pyflakes-ignore-message-ids ",")))))
   (when flymake-mode
     ;; in case this is triggered by mypylint on a buffer not running flymake.
     (flymake-start-syntax-check)))
+
+(defun my-pyflakes-reset-ignore ()
+  (interactive)
+  (setq my-pyflakes-ignore-message-ids nil)
+  (my-pyflakes-build-ignore))
