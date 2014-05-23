a1fe555fb9bcd3ea4c12fc8c93a42f8d64c3b8f7
Nathan Bird <nathan@acceleration.net>
2014-05-23 14:30:50 -0400

    whitespace-cleanup

--- settings/yasnippet-settings.el
+++ settings/yasnippet-settings.el
@@ -2,6 +2,6 @@
 
 (run-with-idle-timer 5 nil 'yas-global-mode)
 (run-with-idle-timer 8 nil '(lambda()
-                              (ensure-packages-installed 'angular-snippets)))
+                             (ensure-packages-installed 'angular-snippets)))
 
 (provide 'yasnippet-settings)
