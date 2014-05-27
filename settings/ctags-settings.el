bf0cf6a2f4350aabeb6482da565903b287fd4916
Nathan Bird <nathan@acceleration.net>
2014-05-27 13:12:35 -0400

    Fix ctags-settings
    Need to require ctags-update before we can set relevant variables.

--- settings/ctags-settings.el
+++ settings/ctags-settings.el
@@ -1,9 +1,11 @@
 
 (ensure-packages-installed 'ctags-update 'ac-etags)
 
-(push "--exclude=bower_components" ctags-update-other-options)
-(push "--exclude=dist" ctags-update-other-options)
-(push "--exclude=build" ctags-update-other-options)
+(eval-after-load "ctags-update"
+  '(progn
+     (push "--exclude=bower_components" ctags-update-other-options)
+     (push "--exclude=dist" ctags-update-other-options)
+     (push "--exclude=build" ctags-update-other-options)))
 
 
 (eval-after-load "etags" '(progn (ac-etags-setup)))
