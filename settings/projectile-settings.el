292a7cd5202740b05d231c47c21bf0b7cda8580e
Nathan Bird <nathan@acceleration.net>
2014-07-15 09:57:06 -0400

    projectile show magit status on project switch
    re Programming:#769

--- settings/projectile-settings.el
+++ settings/projectile-settings.el
@@ -10,6 +10,14 @@
 (setf projectile-remember-window-configs t)
 
 ;; opening a project should show the directory
-(setf projectile-switch-project-action 'projectile-dired)
+(setf projectile-switch-project-action 'projectile-magit-status)
+
+(defun projectile-magit-status ()
+  (or (magit-status (or (projectile-project-root)
+                        (magit-get-top-dir)
+                        (magit-read-top-dir nil))
+                    'switch-to-buffer)
+      (projectile-dired))
+  nil)
 
 (provide 'projectile-settings)
