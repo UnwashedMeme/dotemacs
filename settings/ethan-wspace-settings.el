2590e03e91cee9e1935098dd15b19448d1e9e0da
Nathan Bird <nathan@acceleration.net>
2014-08-12 10:43:38 -0400

    enable global-ethan-wspace-mode more aggressively

--- settings/ethan-wspace-settings.el
+++ settings/ethan-wspace-settings.el
@@ -1,6 +1,7 @@
 (ensure-packages-installed 'ethan-wspace)
 
-(setf mode-require-final-newline nil)
-(run-with-idle-timer 2.2 nil 'global-ethan-wspace-mode)
+;(setf mode-require-final-newline nil)
+(global-ethan-wspace-mode)
+;(run-with-idle-timer 2.2 nil 'global-ethan-wspace-mode)
 
 (provide 'ethand-wspace-settings)
