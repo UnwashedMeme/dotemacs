3d80452a3c00f61d3163ee74b428a19d2a5f9594
Nathan Bird <nathan@acceleration.net>
2014-08-29 12:58:37 -0400

    Turn off ido-flx; it popped to much
    flx-ido-mode is kind of nice in its highlighting but is really
    jumpy in its matching E.g. when typing a filename that is an exact
    match, but with another file in the directory that is a flex
    match, every keystroke flips between the two matches

--- settings/ido-settings.el
+++ settings/ido-settings.el
@@ -1,16 +1,23 @@
-(ensure-packages-installed 'ido-ubiquitous 'flx-ido)
+(ensure-packages-installed 'ido-ubiquitous
+                           ;'flx-ido
+                           )
 
 
-;;(setq ido-enable-flex-matching t)
 ;;(setq ido-mode (quote both) nil (ido))
 ;;(setq ido-enable-flex-matching t)
 
 (ido-mode 1)
 (ido-everywhere 1)
-(flx-ido-mode 1)
+(setq ido-enable-flex-matching t)
+
+;;; flx-ido-mode is kind of nice in its highlighting but is really
+;;; jumpy in its matching E.g. when typing a filename that is an exact
+;;; match, but with another file in the directory that is a flex
+;;; match, every keystroke flips between the two matches
+;(flx-ido-mode 1)
 ;; disable ido faces to see flx highlights.
-(setq ido-use-faces nil)
-(setq flx-ido-use-faces t)
+;(setq ido-use-faces nil)
+;(setq flx-ido-use-faces t)
 
 
 ;;; when typing a new filename don't suggest a different directory's
