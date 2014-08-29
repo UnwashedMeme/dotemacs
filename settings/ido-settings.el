49ce8a75126af1bd32632dc1e5489c65dcca16b3
Nathan Bird <nathan@acceleration.net>
2014-08-29 13:54:53 -0400

    flx-ido fixed the stability issues; reenable
    this also makes it easier to enable/disable it.

--- settings/ido-settings.el
+++ settings/ido-settings.el
@@ -1,23 +1,27 @@
-(ensure-packages-installed 'ido-ubiquitous
-                           ;'flx-ido
-                           )
-
-
-;;(setq ido-mode (quote both) nil (ido))
-;;(setq ido-enable-flex-matching t)
+(ensure-packages-installed 'ido-ubiquitous)
 
 (ido-mode 1)
 (ido-everywhere 1)
-(setq ido-enable-flex-matching t)
 
-;;; flx-ido-mode is kind of nice in its highlighting but is really
-;;; jumpy in its matching E.g. when typing a filename that is an exact
-;;; match, but with another file in the directory that is a flex
-;;; match, every keystroke flips between the two matches
-;(flx-ido-mode 1)
-;; disable ido faces to see flx highlights.
-;(setq ido-use-faces nil)
-;(setq flx-ido-use-faces t)
+(defun enable-flx-ido ()
+  (ensure-packages-installed 'flx-ido)
+  (setq ido-enable-flex-matching nil)
+  ;; flx-ido-mode is kind of nice in its highlighting but is really
+  ;; jumpy in its matching E.g. when typing a filename that is an exact
+  ;; match, but with another file in the directory that is a flex
+  ;; match, every keystroke flips between the two matches
+  (flx-ido-mode 1)
+  ;; disable ido faces to see flx highlights.
+  (setq ido-use-faces nil)
+  (setq flx-ido-use-faces t)
+)
+
+(defun disable-flx-ido ()
+  (flx-ido-mode 0)
+  (setq ido-enable-flex-matching t)
+  (setq ido-use-faces t)
+)
+(enable-flx-ido)
 
 
 ;;; when typing a new filename don't suggest a different directory's
