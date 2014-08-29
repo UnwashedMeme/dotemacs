d514bbd37e8073b1f6f5c49c89fe7b0d728cc002
Nathan Bird <nathan@acceleration.net>
2014-08-29 15:17:46 -0400

    Fix smartparens typo

--- settings/smartparens-settings.el
+++ settings/smartparens-settings.el
@@ -7,7 +7,7 @@
 ;; If non-nil, autoskip of closing pair is cancelled not only
 ;;when point is moved outside of the pair, but also if the point
 ;; moved backwards.  See `sp-skip-closing-pair' for more info.
-(setf sp-cancel-autoskip-on-backward-movemeqnt nil)
+(setf sp-cancel-autoskip-on-backward-movement nil)
 
 
 ;(define-key sp-keymap (kbd "<C-right>") 'sp-forward-sexp)
