cc1febde1c4e3b7b459fe36456b4e7f1237acb06
Ryan Davis <ryan@acceleration.net>
2014-09-08 09:17:44 -0400

    add some lispy key bindings
    Trying to retain backwards compatibility with the bindings setup by
    adwcodebase config that we've been using for years.

--- settings/smartparens-settings.el
+++ settings/smartparens-settings.el
@@ -4,12 +4,29 @@
 (smartparens-global-mode t)
 (show-smartparens-global-mode t)
 
-;; If non-nil, autoskip of closing pair is cancelled not only
-;;when point is moved outside of the pair, but also if the point
-;; moved backwards.  See `sp-skip-closing-pair' for more info.
+;; If non-nil, autoskip of closing pair is cancelled not only when
+;; point is moved outside of the pair, but also if the point moved
+;; backwards.  See `sp-skip-closing-pair' for more info.
 (setf sp-cancel-autoskip-on-backward-movement nil)
 
+; lisp-ish mode key bindings
+(when (fboundp 'add-key-to-lisp-maps)
+  (add-key-to-lisp-maps (kbd "C-t") 'sp-transpose-sexp)
+  (add-key-to-lisp-maps (kbd "C-M-t") 'transpose-chars)
+  (add-key-to-lisp-maps (kbd "C-M-k") 'kill-line)
+  (add-key-to-lisp-maps (kbd "C-k") 'sp-kill-sexp)
+  (add-key-to-lisp-maps (kbd "C-<right>") 'sp-forward-sexp)
+  (add-key-to-lisp-maps (kbd "C-<left>") 'sp-backward-sexp)
 
-;(define-key sp-keymap (kbd "<C-right>") 'sp-forward-sexp)
-;(define-key sp-keymap (kbd "<C-left>") 'sp-backward-sexp)
+  ;; (add-key-to-lisp-maps (kbd "C-S-<right>")
+  ;;                       (lambda ()
+  ;;                         (interactive)
+  ;;                         (mark-sexp)))
+  ;; (add-key-to-lisp-maps (kbd "C-S-<left>")
+  ;;                       (lambda ()
+  ;;                         (interactive)
+  ;;                         (mark-sexp -1)))
+  )
+
+
 (provide 'smartparens-settings)
