3336b842af20c729edcaaa553eb63cf87ee664e9
Nathan Bird <nathan@acceleration.net>
2015-03-03 14:54:42 -0500

    Fix indent-tabs-mode nil
    This became a buffer local variable at some point and our changes got
    lost
    
    re Programming:#769 (0.1)

--- settings/ethan-wspace-settings.el
+++ settings/ethan-wspace-settings.el
@@ -4,6 +4,7 @@
 (global-ethan-wspace-mode)
 ;(run-with-idle-timer 2.2 nil 'global-ethan-wspace-mode)
 
-(setf indent-tabs-mode nil)
+;; this becomes buffer local when set so need to use setq-default
+(setq-default indent-tabs-mode nil)
 
 (provide 'ethand-wspace-settings)
