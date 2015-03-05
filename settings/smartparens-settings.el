bbfd0b22171c05daad342edc053067f3dd0c6d8d
Russ Tyndall <russ@acceleration.net>
2015-03-05 15:56:27 -0500

    Fix forward/backward sexp selection in smart paren

--- settings/smartparens-settings.el
+++ settings/smartparens-settings.el
@@ -28,5 +28,78 @@
   ;;                         (mark-sexp -1)))
   )
 
+;; TODO: Remove these function definitions when upstream is updated
+;; these were added to allow shift selcting forward / backward sexps
+;; which I could not work without the difference
+;; is the carat in (interactive "^p")
+;; https://github.com/Fuco1/smartparens/issues/360
+;;
+
+(put 'sp-forward-sexp  'CUA 'move)
+(put 'sp-backward-sexp 'CUA 'move)
+(defun sp-forward-sexp (&optional arg)
+  "Move forward across one balanced expression.
+
+With ARG, do it that many times.  Negative arg -N means move
+backward across N balanced expressions.  If there is no forward
+expression, jump out of the current one (effectively doing
+`sp-up-sexp').
+
+With `sp-navigate-consider-symbols' symbols and strings are also
+considered balanced expressions.
+
+Examples: (prefix arg in comment)
+
+  |(foo bar baz)   -> (foo bar baz)|
+
+  (|foo bar baz)   -> (foo| bar baz)
+
+  (|foo bar baz)   -> (foo bar| baz) ;; 2
+
+  (foo (bar baz|)) -> (foo (bar baz)|)"
+  (interactive "^p")
+  (setq arg (or arg 1))
+  (if (< arg 0)
+      (sp-backward-sexp (- arg))
+    (let* ((n arg)
+           (ok t))
+      (while (and ok (> n 0))
+        (setq ok (sp-get-thing))
+        (setq n (1- n))
+        (when ok (goto-char (sp-get ok :end))))
+      ok)))
+
+(defun sp-backward-sexp (&optional arg)
+  "Move backward across one balanced expression (sexp).
+
+With ARG, do it that many times.  Negative arg -N means move
+forward across N balanced expressions.  If there is no previous
+expression, jump out of the current one (effectively doing
+`sp-backward-up-sexp').
+
+With `sp-navigate-consider-symbols' symbols and strings are also
+considered balanced expressions.
+
+Examples: (prefix arg in comment)
+
+  (foo bar baz)|   -> |(foo bar baz)
+
+  (foo| bar baz)   -> (|foo bar baz)
+
+  (foo bar| baz)   -> (|foo bar baz) ;; 2
+
+  (|(foo bar) baz) -> ((|foo bar) baz)"
+  (interactive "^p")
+  (setq arg (or arg 1))
+  (if (< arg 0)
+      (sp-forward-sexp (- arg))
+    (let* ((n arg)
+           (ok t))
+      (while (and ok (> n 0))
+        (setq ok (sp-get-thing t))
+        (setq n (1- n))
+        (when ok (goto-char (sp-get ok :beg))))
+      ok)))
+
 
 (provide 'smartparens-settings)
