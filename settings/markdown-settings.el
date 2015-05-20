c3862eca1cf883829222607fd174cb5ad5968d55
Nathan Bird <nathan@acceleration.net>
2015-05-20 14:41:09 -0400

    Use 'marked' for markdown if installed

--- settings/markdown-settings.el
+++ settings/markdown-settings.el
@@ -1,9 +1,23 @@
 (ensure-packages-installed 'markdown-mode 'markdown-toc)
 
-;;markdown-toc doesn't define its own autoloads properly here; do it
-;;manually.
-(autoload 'markdown-toc/generate-toc "markdown-toc" nil t)
-(autoload 'markdown-toc/version "markdown-toc" nil t)
+(use-package markdown-mode
+             :ensure markdown-mode
+             :mode "\\.md\\'"
+             :config (progn
+                       (when (executable-find "marked")
+                         (setf markdown-command "marked"))
+                       (setf markdown-css-path
+                             "https://raw.githubusercontent.com/gollum/gollum/master/lib/gollum/public/gollum/css/template.css"
+                                ;"http://kevinburke.bitbucket.org/markdowncss/markdown.css"
+                             )))
 
-
+(use-package markdown-toc
+             :ensure markdown-toc
+             :defer t
+             :init (progn
+                     ;;markdown-toc doesn't define its own autoloads properly here; do it
+                     ;;manually.
+                     (autoload 'markdown-toc/generate-toc "markdown-toc" nil t)
+                     (autoload 'markdown-toc/version "markdown-toc" nil t)
+                     ))
 (provide 'markdown-settings)
