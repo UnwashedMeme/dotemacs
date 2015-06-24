a60244829d33633154c2ae9854b4c50830e86354
Nathan Bird <nathan@acceleration.net>
2015-06-24 15:03:35 -0400

    Try and make apache mode autoload correctly
    I'm still not sure this is working...

--- settings/apache-settings.el
+++ settings/apache-settings.el
@@ -1,2 +1,6 @@
-(ensure-packages-installed 'apache-mode)
+(use-package apache-mode
+             :init (autoload 'apache-mode "apache-mode")
+             :commands apache-mode
+             :ensure apache-mode)
+
 (provide 'apache-settings)
