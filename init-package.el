527ad2735efe601a112ebc73ef028934a049b367
Nathan Bird <nbird@acis.ufl.edu>
2016-03-31 09:11:07 -0400

    Deactivate marmalade
    Not using any packages from there; pretty much everything is on melpa
    and marmalade is normally out of date.

--- init-package.el
+++ init-package.el
@@ -2,7 +2,7 @@
 (setq package-archives
    (quote
     (("gnu" . "https://elpa.gnu.org/packages/")
-     ("marmalade" . "https://marmalade-repo.org/packages/")
+;     ("marmalade" . "https://marmalade-repo.org/packages/")
      ("elpy" . "http://jorgenschaefer.github.io/packages/")
      ("melpa" . "http://melpa.milkbox.net/packages/"))))
 
