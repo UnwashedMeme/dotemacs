4f6987ac80e3d23427a576b5dcb222ee24a7cd3c
Nathan Bird <nbird@acis.ufl.edu>
2016-07-27 16:14:20 -0400

    Use flycheck for python

--- settings/flycheck-settings.el
+++ settings/flycheck-settings.el
@@ -1,8 +1,8 @@
 (ensure-packages-installed 'flycheck)
 
-(setq flycheck-locate-config-file-functions
-      '(flycheck-locate-config-file-home
-        flycheck-locate-config-file-ancestor-directories
-        flycheck-locate-config-file-absolute-path))
+;(setq flycheck-locate-config-file-functions
+;      '(flycheck-locate-config-file-home
+;        flycheck-locate-config-file-ancestor-directories
+;        flycheck-locate-config-file-absolute-path))
 
 (provide 'flycheck-settings)
--- settings/python-settings.el
+++ settings/python-settings.el
@@ -17,56 +17,27 @@
     (require 'elpy)))
 
 (use-package elpy
-             :ensure elpy
+             :ensure t
              :defer t
              :config
              (progn
                (elpy-enable)
-               (elpy-use-ipython)
                (setq elpy-test-runner 'elpy-test-pytest-runner)
-               (define-key elpy-mode-map [C-right] 'right-word)
-               (define-key elpy-mode-map [C-left] 'left-word)))
 
-(use-package mypylint
-             :ensure python-pylint
-             :commands mypylint)
+               (when (require 'flycheck nil t)
+                 (message "Enabling flycheck/elpy integration")
+                 (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+                 (add-hook 'elpy-mode-hook 'flycheck-mode)
+                 (define-key elpy-mode-map "\C-c\C-n" 'flycheck-next-error)
+                 (define-key elpy-mode-map "\C-c\C-p" 'flycheck-previous-error))
+               (elpy-use-ipython)
 
-;; elpy handles this
-;; (add-hook 'python-mode-hook 'ctags-update-minor-mode)
+               )
+             :bind ((:map elpy-mode-map
+                          ([C-right] . right-word)
+                          ([C-left] . left-word))))
 
+(setenv "IPY_TEST_SIMPLE_PROMPT" "1")
 
-;; (setf elpy-rpc-backend nil)
-;; (defadvice elpy-rpc--open (around native-rpc-for-tramp activate)
-;;   (interactive)
-;;   (let ((elpy-rpc-backend
-;;          (if (ignore-errors (tramp-tramp-file-p (elpy-project-root)))
-;;              "native"
-;;            elpy-rpc-backend)))
-;;      (message "Using elpy backend: %s for %s" elpy-rpc-backend (elpy-project-root))
-;;      ad-do-it))
-
-
-;(define-key elpy-mode-map [C-up] 'backward-paragraph)
-;(define-key elpy-mode-map [C-down] 'forward-paragraph)
-
-
-
-;;;; pyflakes ELPY does pretty well with pyflakes/flake8 by itself;
-;;;; this adds a bit of ignore logic but...
-
-;; (use-package flymake-python-pyflakes
-;;              :ensure flymake-python-pyflakes
-;;              :defer t
-;;              :config (setf flymake-python-pyflakes-executable "flake8"))
-
-
-;; (defun maybe-flymake-activate ()
-;;   (cond ((not (tramp-handle-file-remote-p (buffer-file-name)))
-;;          (message "Activating flymake-python-pyflakes")
-;;          (flymake-python-pyflakes-load)
-;;          (message "Activing custom navigation minor-mode.")
-;;          (my-pyflakes-minor-mode))
-;;         (t
-;;          (message "Skipping flymake-python-pyflakes for remote tramp buffer."))))
 
 (provide 'python-settings)
