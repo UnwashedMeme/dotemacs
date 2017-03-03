f8066884a57b88714eae4f3a32262c8e1d9ccf56
Nathan Bird <nbird@acis.ufl.edu>
2017-03-03 08:35:24 -0500

    Setup ripgrep

---
+++ settings/ripgrep-settings.el
@@ -0,0 +1,19 @@
+;; ripgrep-search-mode
+
+(use-package ripgrep
+  :if (executable-find "rg")
+  :commands (ripgrep-regexp)
+  :ensure t
+  :bind (:map ripgrep-search-mode-map
+              ("n" . next-error-no-select)
+              ("p" . previous-error-no-select)))
+(use-package projectile-ripgrep
+  :if (executable-find "rg")
+  :ensure t
+  :commands (projectile-ripgrep)
+  :bind (:map projectile-mode-map
+              ("C-c p s r" . projectile-ripgrep))
+  :config (require 'projectile-settings))
+
+
+(provide 'ripgrep-settings)
